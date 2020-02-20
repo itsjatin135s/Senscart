5fff72d222dd8d6f32068b2004a17f5c02d33fff
jatinsaini137 <jatinrajbagri2001@gmail.com>
2020-02-20 23:35:58 +0530

    shopperstop

--- mainapp/__init__.py
+++ mainapp/__init__.py
@@ -1,14 +1,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
-import os
 
 
 
-
-file_path = os.path.abspath(os.getcwd())+"/test.db"
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'ad66d90a56b675248acb31307cca3cc8017fc2ac'
