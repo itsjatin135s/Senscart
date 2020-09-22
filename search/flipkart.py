c5238e3b7099ace8beae868aabe2a984fa9d3a0f
jatinsaini137 <jatinrajbagri2001@gmail.com>
2020-09-22 15:36:33 +0530

    new flipkart file

--- search/flipkart.py
+++ search/flipkart.py
@@ -22,18 +22,13 @@ def flipkart(query):
 
 		# script=query("#is_script").text()
 		headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.3; WOW64; rv:57.0) Gecko/20100101 Firefox/57.0'}
-		flipkarts = requests.get("https://www.flipkart.com/search?q={}".format(query),headers=headers)
-		script1 = soup(flipkarts.text,'lxml')
-		script= script1.find('script', {"id":"is_script"}).text
+		f=requests.get("https://flipkart.com/search?q={}".format(query),headers=headers)
+		ft=soup(f.content,'lxml')
+		fk=ft.find('script',id='is_script').text
 
-
-		remove1=script.replace('window.__INITIAL_STATE__ = ','')
-		remove2=remove1[:-1]
+		remove1=fk.replace('window.__INITIAL_STATE__ = ','')
+		remove2=remove1[:-2]
 		k1=json.loads(remove2)
-
-		# remove1=k.replace('window.__INITIAL_STATE__ = ','')
-		# remove2=remove1[:-1]
-		# k1=json.loads(remove2)
 		# print(k1)
 		for x in range(1,11):
 			j=(k1['pageDataV4']['page']['data']['10003'][x]['widget']['data']['products'][0]['productInfo']['value']['media']['images'][0]['url'])
@@ -62,7 +57,7 @@ def flipkart(query):
 			print(o)
 
 	except:
-		print(script)
+		# print(script)
 		errorimg=url_for('static',filename='images/error.png')
 		imglink.append(errorimg)
 		prices.append("Unavilable")
