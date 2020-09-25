c2298609d27529a06d6013a6f5953a8a1623a5b8
jatinsaini137 <jatinrajbagri2001@gmail.com>
2020-09-25 12:00:21 +0530

    checking on error 503 response in amazon

--- search/amazon.py
+++ search/amazon.py
@@ -18,6 +18,7 @@ def amazon(rawquery):
 		headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.3; WOW64; rv:57.0) Gecko/20100101 Firefox/57.0'}
 		amazons = requests.get("https://www.amazon.in/s?k={}".format(query),headers=headers,verify=False)
 		# amazons=urlopen('https://amazon.in/s?k=iphone').read()
+		print(amazons)
 		soup1=soup(amazons.text,'lxml')
 		
 
